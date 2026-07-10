8cbdfb4181814ebf9f023fb5e605bbf08ed23138
Flaouve <flaouve@gmail.com>
2026-07-10 17:16:25 +0300

    feat: add automatic rich text tags generation for TMP color, size, and alignment in Text block

--- Assets/DONT TOUCH/Scripts/BlockComponents/TextComponent.cs
+++ Assets/DONT TOUCH/Scripts/BlockComponents/TextComponent.cs
@@ -26,9 +26,27 @@ public class TextComponent : SchematicBlock
 
 	public override void Compile(SchematicBlockData block)
 	{
+		string formattedText = _textMesh.text;
+
+		// Wrap in size tag
+		formattedText = $"<size={_textMesh.fontSize}>{formattedText}</size>";
+
+		// Wrap in color tag
+		formattedText = $"<color=#{ColorUtility.ToHtmlStringRGBA(_textMesh.color)}>{formattedText}</color>";
+
+		// Wrap in alignment tag
+		string align = _textMesh.alignment switch
+		{
+			TextAlignmentOptions.Left or TextAlignmentOptions.TopLeft or TextAlignmentOptions.BottomLeft or TextAlignmentOptions.BaselineLeft or TextAlignmentOptions.MidlineLeft => "left",
+			TextAlignmentOptions.Right or TextAlignmentOptions.TopRight or TextAlignmentOptions.BottomRight or TextAlignmentOptions.BaselineRight or TextAlignmentOptions.MidlineRight => "right",
+			TextAlignmentOptions.Justified or TextAlignmentOptions.TopJustified or TextAlignmentOptions.BottomJustified or TextAlignmentOptions.BaselineJustified or TextAlignmentOptions.MidlineJustified => "justified",
+			_ => "center"
+		};
+		formattedText = $"<align={align}>{formattedText}</align>";
+
 		block.Properties = new Dictionary<string, object>
 		{
-			{ "Text", _textMesh.text },
+			{ "Text", formattedText },
 			{ "DisplaySize", (SerializableVector)_textMesh.rectTransform.sizeDelta }
 		};
 
@@ -40,7 +58,42 @@ public class TextComponent : SchematicBlock
 		TMP_Text text = Create<GameObject>("Assets/Resources/Blocks/Text.prefab").GetComponent<TMP_Text>();
 		gameObject = text.gameObject;
 
-		text.text = Convert.ToString(block.Properties["Text"]);
+		string rawText = Convert.ToString(block.Properties["Text"]);
+
+		// Parse alignment
+		var alignMatch = System.Text.RegularExpressions.Regex.Match(rawText, @"<align=(left|right|center|justified)>");
+		if (alignMatch.Success)
+		{
+			string align = alignMatch.Groups[1].Value;
+			text.alignment = align switch
+			{
+				"left" => TextAlignmentOptions.CenterLeft,
+				"right" => TextAlignmentOptions.CenterRight,
+				"justified" => TextAlignmentOptions.Justified,
+				_ => TextAlignmentOptions.Center
+			};
+			rawText = System.Text.RegularExpressions.Regex.Replace(rawText, @"</?align[^>]*>", "");
+		}
+
+		// Parse color
+		var colorMatch = System.Text.RegularExpressions.Regex.Match(rawText, @"<color=#([A-Fa-f0-9]{8})>");
+		if (colorMatch.Success)
+		{
+			if (ColorUtility.TryParseHtmlString("#" + colorMatch.Groups[1].Value, out Color color))
+				text.color = color;
+			rawText = System.Text.RegularExpressions.Regex.Replace(rawText, @"</?color[^>]*>", "");
+		}
+
+		// Parse size
+		var sizeMatch = System.Text.RegularExpressions.Regex.Match(rawText, @"<size=([0-9\.]+)>");
+		if (sizeMatch.Success)
+		{
+			if (float.TryParse(sizeMatch.Groups[1].Value, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out float size))
+				text.fontSize = size;
+			rawText = System.Text.RegularExpressions.Regex.Replace(rawText, @"</?size[^>]*>", "");
+		}
+
+		text.text = rawText;
 		text.rectTransform.sizeDelta = JsonConvert.DeserializeObject<Vector2>(block.Properties["DisplaySize"].ToString());
 
 		base.Decompile(ref gameObject, block, parent);
